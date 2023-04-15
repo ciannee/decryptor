adcce56029b60a26a51d89f0941070c240c87b24
Patricia Pineda <pepineda28@gmail.com>
2023-04-16 00:12:29 +0800

    e is working

--- decrypt.py
+++ decrypt.py
@@ -25,7 +25,8 @@ while decryption_str != "no":
         if input_str[i] == "a":
             output_str += "*"
     #   if e, change to &
-       
+        elif input_str[i] == "e":
+            output_str += "&"
     #   if i, change to #
         
     #   if o, change to +
