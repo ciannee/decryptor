3b8ca7b76defc1d8db51179ba67d62edad130b35
Patricia Pineda <pepineda28@gmail.com>
2023-04-15 23:43:33 +0800

    pseudocode

---
+++ decrypt.py
@@ -0,0 +1,28 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 3: Redo Problem 2 with multiple github commits (Decryption)
+
+# looping
+   
+# ask for user's input
+    # check each character
+
+    #   if a, change to *
+        
+    #   if e, change to &
+       
+    #   if i, change to #
+        
+    #   if o, change to +
+        
+    #   if u, change to !  
+        
+    # print output and loading screen 
+   
+    # decryption
+
+    # print output
+
+    # import pyfiglet module
+   
+    # looping
